0206714123a00466798e615ca9d23799e730bbfd
Ventricule <pierretandille@gmail.com>
2015-04-07 18:05:43 +0200

    Ajout de mon compte

---
+++ site/accounts/pierre.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: pierre
+email: pierretandille@gmail.com
+password: >
+  $2a$10$3eHliB.b2mPl7XJZRbmGjuqNwxs3QT3qSdzAcqACk1BdTxi9qb8fa
+language: fr
+role: admin
+token: 8affed0d5cac5c0ce95ec440b93808bc45f9106c
