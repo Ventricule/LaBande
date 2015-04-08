2b3a84b14a6da42baa88ddf66b1700ce069abea7
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-04-08 11:10:12 +0200

    nouveau compte samuel

---
+++ site/accounts/samuel.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: samuel
+email: samuel.riversmoore@gmail.com
+password: >
+  $2a$10$KKCYWkQYe8doO2WLNQ/CpO6ckXR.uJp39i8Q60AHVY8evooiiulB6
+language: fr
+role: admin
+history:
+  - projects/project-a
+token: 34714157b39214cde715e3349f59dcea186578de
