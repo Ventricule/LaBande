c058eee55c55bad726d96319f97f12870b6340a0
Ventricule <pierretandille@gmail.com>
2015-04-13 18:00:26 +0200

    Adding files for the map

--- site/accounts/pierre.php
+++ site/accounts/pierre.php
@@ -6,4 +6,4 @@ password: >
   $2a$10$3eHliB.b2mPl7XJZRbmGjuqNwxs3QT3qSdzAcqACk1BdTxi9qb8fa
 language: fr
 role: admin
-token: 8affed0d5cac5c0ce95ec440b93808bc45f9106c
+token: 2e1ff4cc9d33cc48765c6a4817d0511a282e8974
