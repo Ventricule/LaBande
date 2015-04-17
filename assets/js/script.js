9e003e64ec48324b3fda14ee0caa4cc1b310bade
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-04-17 15:59:20 +0200

    ok menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,25 +14,28 @@
 			color += letters[Math.floor(Math.random() * letters.length)];
 			return color
 		};
-    
-		var menu = new Bande($('#bande1') );
+
+		$('nav').find('li').each(function(){
+			$(this).css('background-color', colors);
+			if ($(this).css('background-color') === 'rgb(66, 34, 34)' || $(this).css('background-color') === 'rgb(0, 0, 255)' ){
+				$(this).css('color', '#FFFFFF');
+			}
+		});
+
+		var menu = new Bande($('#bande1'));
 		var submenu = new Bande($('#bande2'));
-    
     
 
 		function Bande(container) {
 			this.container = container;
 			var parent = container.find('ul'), childs = container.find('ul > li'), listLength = childs.length;
-			var oldVal, clicked, direction, mySpeed = 500;
+			var oldVal, direction, mySpeed = 500;
 
 			this.swiper = new Swiper(container, {
-				onInit: function(swiper){
-          var etiquetteOriginale = swiper.container.find('li[data-swiper-slide-index="0"]');
-          console.log(etiquetteOriginale);
-          $('nav').css('background-color', colors);
-					$('nav').find('li').each(function(){
-						$(this).css('background-color', colors);
-					});
+				onInit: function(){
+					if (listLength <= 5){
+						childs.clone().appendTo(parent);
+					};
 				},
 				slidesPerView: 'auto',
 				slideToClickedSlide: true,
@@ -42,47 +45,36 @@
 				resistanceRatio : 0.9,
 				loop: true,
 				loopedSlides: listLength,
-        centeredSlides: true,
 				slideActiveClass: 'active',
+				slideDuplicateClass: 'duplicate',
+				runCallbacksOnInit: false,
 				onSlideChangeStart: function(swiper){
-					clicked = swiper.clickedIndex;
+					swiper.update();
+					var activeSlide = $(swiper.wrapper).find('.active');
+					slideMenu(swiper, activeSlide.attr('data-hash'), activeSlide.attr('data-p-hash'));  
 				},
 				onSetTranslate: function(swiper, translate){
 					// check direction on translate
 					if (swiper.translate < oldVal){ direction = 'right'; } else if (swiper.translate > oldVal) { direction = 'left'; } 
 					oldVal = swiper.translate;
 				},
-				onSetTransition: function(swiper){
-
-				},
-				onSlideChangeEnd: function(swiper){
-          var activeSlide = $(swiper.wrapper).find('.active');
-          swiper.slideTo( parseInt(activeSlide.attr('data-swiper-slide-index'))+listLength, 0, false);
-					var hash = activeSlide.attr('data-hash');
-					var parentHash = activeSlide.attr('data-p-hash');
-					slideMenu(swiper, hash, parentHash);  
+				onTransitionEnd: function(swiper){
+					var activeSlide = $(swiper.wrapper).find('.active');
+					swiper.slideTo( parseInt(activeSlide.attr('data-swiper-slide-index'))+listLength, 0, false);
+					swiper.update();
 				} 
 			});
 
 			function slideMenu(swiper, hash, parentHash) {
 				if(parentHash && menu) {
-					var parent = menu.container.find('li[data-hash='+parentHash+']');
+					var parent = menu.container.find('li[data-hash='+parentHash+']:not(.duplicate)');
 					menu.swiper.slideTo(parent.index(), 1000, false);
 				} else if (submenu) {
-					var firstChild = submenu.container.find('li[data-p-hash='+hash+']').first();
+					var firstChild = submenu.container.find('li[data-p-hash='+hash+']:not(.duplicate)').first();
 					submenu.swiper.slideTo(firstChild.index(), 1000, false);
 				}
 			}
 		};
-
-
-		$('nav').find('li').each(function(){
-			if ($(this).css('background-color') === 'rgb(66, 34, 34)' || $(this).css('background-color') === 'rgb(0, 0, 255)' ){
-				$(this).css('color', '#FFFFFF');
-			}
-		});
-
-
 
 
 
