0d1b237887aa7cbe5d70604839a8e7bd6ae9bdd6
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-04-28 13:08:39 +0200

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
 					console.log('init');
 				};
 				direction = 'prev';
-				swiper.update(true);
+				setTimeout(function(){ swiper.update(true);}, 300);
 				console.log('yop');
 			},
 			slidesPerView: 'auto',
