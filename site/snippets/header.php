9d90fe73cef386fe6e4a2b4534a83978dd1ff984
Ventricule <pierretandille@gmail.com>
2015-04-28 19:19:58 +0200

    oublié la virgoule

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,7 +14,7 @@
 		'assets/css/main.css',
 		'assets/css/entypo.css',
 		'assets/swiper/css/swiper.min.css',
-    'https://api.tiles.mapbox.com/mapbox.js/v2.1.8/mapbox.css'
+    'https://api.tiles.mapbox.com/mapbox.js/v2.1.8/mapbox.css',
 		'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css',
 		'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css'
 	)) ?>
