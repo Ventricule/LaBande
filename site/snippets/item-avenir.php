a2eac08ea1a734be8b95825c974959a6a4b1057c
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2015-04-29 18:00:21 +0200

    update avenir

--- site/snippets/item-avenir.php
+++ site/snippets/item-avenir.php
@@ -1,22 +1,33 @@
-<h3 class="entry-title" ><a href="<?php echo $entry->url() ?>"><?php echo $entry->title()->html() ?></a></h3>
+<!-- <h3 class="entry-title" ><a href="<?php echo $entry->url() ?>"><?php echo $entry->title()->html() ?></a></h3> -->
 
+<p><?php echo $entry->text() ?></p>
+
+
+<h3 class="avenir-title">Événements à venir :</h3>
 
 <ul class="avenir">
-	<?php foreach( page('manifestations')->children()->visible()->filterBy('date', '>', time())->sortBy('date', 'asc')->limit( $entry->number()->int() ) as $subpage): ?>
+	<?php foreach( page('manifestations')->children()->visible()->filterBy('date', '>', time())->sortBy('date', 'asc')->limit( $entry->number()->int() ) as $manifestation): ?>
 		<li class='item-avenir'>
-			<h4><?php echo html($subpage->title()) ?></h4>
+			<h4><?php echo html($manifestation->title()) ?></h4>
+			
+			<?php
+				$date =	ucfirst(strftime('%A %d %B %Y', $manifestation->date()));
+				$heure = str_replace(":","h",$manifestation->time());
+			?>
 
-			<div class="meta-avenir entry-date icon-calendar icon-medium"><?php echo $subpage->date('d.m.Y') ?></spdivan> 
+			<div class="entry-date icon-calendar"><?php echo $date ?></div> 
+			<div class="entry-time icon-clock"><?php echo $heure ?></div>
 
-<!--			<?php if($image = $subpage->images()->sortBy('sort', 'asc')->first()): ?>
+
+<!--			<?php if($image = $manifestation->images()->sortBy('sort', 'asc')->first()): ?>
 				<figure class="head preview">
-					<img src="<?php echo $image->url() ?>" alt="<?php echo $subpage->title()->html() ?>" >
+					<img src="<?php echo $image->url() ?>" alt="<?php echo $manifestation->title()->html() ?>" >
 				</figure>
 			<?php endif ?>
 -->
 			<?php 
-				$lieu = $subpage->lieu();
-				$price = (string)$subpage->price();
+				$lieu = $manifestation->lieu();
+				$price = (string)$manifestation->price();
 				$lieuName = $adress = $transport = '';
 				if ( (string)$lieu ) {
 					$lieu = page('lieux')->children()->find($lieu);
@@ -24,8 +35,8 @@
 					$adress = (string)$lieu->adress();
 					$transport = (string)$lieu->transport();
 				} else {
-					$adress = (string)$subpage->locationName();
-					$transport = (string)$subpage->transport();
+					$adress = (string)$manifestation->locationName();
+					$transport = (string)$manifestation->transport();
 				}
 			?>
 			<?php if($lieuName){ ?><div class="icon-home"><?php echo $lieuName ?></div><?php } ?>
